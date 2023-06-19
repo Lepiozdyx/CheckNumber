4a03e7f45c498d68b3dfe00e6b21e7ad943943a5
Alex Zhulai <Lepiozdyx@gmail.com>
2023-06-19 15:24:56 -0300

    Initial Commit

---
+++ CheckNumber/CheckNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckNumberApp.swift
+//  CheckNumber
+//
+//  Created by Alex on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CheckNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

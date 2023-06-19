9d619aad21798b6c3dfbacda5b755c4bb2a1905c
Alex Zhulai <Lepiozdyx@gmail.com>
2023-06-19 15:50:58 -0300

    Add basic user interface elements

--- CheckNumber/ContentView.swift
+++ CheckNumber/ContentView.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var number = Double.random(in: 0...100).rounded()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 40) {
+            Text("Подвиньте слайдер как можно ближе к: \(lround(number))")
+            HStack {
+                Text("0")
+                Slider(value: $number, in: 0...100, step: 1)
+                Text("100")
+            }
+            Button("Проверь меня!", action: {})
+            Button("Начать заново", action: {})
         }
         .padding()
     }
